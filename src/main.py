8a274d47bd6a94ee5c65939d8ba3b0663c97b520
Edgar Eler <eler@edgar.systems>
2025-07-18 16:46:53 -0300

    Create method to load the RAG index

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-from typing import Union
 from fastapi import FastAPI
 from src.chats.router import router as router_chats
 from src.messages.router import router as router_messages
+from src.llm.llm import load_model, warm_up
+from src.rag.load import load_index, load_retriever
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  print("Starting up...")
+  load_model()
+  print("Warming up model")
+  warm_up()
+  print("Model warmed up")
+  load_index()
+  load_retriever()
+  yield
+  print("Shutting down...")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
---
+++ src/rag/load.py
@@ -0,0 +1,43 @@
+import chromadb
+from pathlib import Path
+from llama_index.core import VectorStoreIndex, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+script_directory = Path(__file__).parent.resolve()
+
+index = None
+query_engine = None
+retriever = None
+
+def load_index():
+  global index
+  if index == None:
+    print("Loading embedding model...")
+
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+    Settings.llm = None
+
+    print("Loading RAG index...")
+    persist_dir = f"{script_directory}/../../storage"
+
+    chroma_client = chromadb.PersistentClient(path=f"{persist_dir}/chroma_db")
+    chroma_collection = chroma_client.get_or_create_collection("exoplanets")
+
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    index = VectorStoreIndex.from_vector_store(
+      vector_store,
+      embed_model=Settings.embed_model,
+    )
+    print("RAG index loaded.")
+  return index
+
+def load_retriever():
+  global retriever
+  if retriever == None:
+    index = load_index()
+    print("Loading retriever...")
+    retriever = index.as_retriever(similarity_top_k=4)
+    print("Retriever loaded")
+  return retriever
