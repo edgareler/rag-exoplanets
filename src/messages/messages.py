c35344a39edf2d00ae5c8229e153266ddc2d4774
Edgar Eler <eler@edgar.systems>
2025-07-19 08:46:29 -0300

    Build context and final prompt on Message creation

--- src/messages/messages.py
+++ src/messages/messages.py
@@ -1,12 +1,16 @@
 import asyncio
+from datetime import datetime
 from enum import Enum
 from fastapi import HTTPException
-from nanoid import generate
+from nanoid import generate as nanoid
 from dataclasses import dataclass
 from src.database.client import get_client
 from src.database.tables import MESSAGES_TABLE
-from src.database.utils import page_to_range, build_response
+from src.database.utils import page_to_range, build_response, DBResponse
 from src.chats.chats import read_chat, Chat
+from src.rag.query import question
+from src.llm.llm import generate
+from src.llm.prompt import build_prompt
 
 class MessageType(str, Enum):
   user = 'user'
@@ -21,28 +25,59 @@ class Message:
   id: str | None = None
 
 async def create_message(message: Message):
+  user_message_date = datetime.now().isoformat()
+
   chat = await read_chat(Chat(
     message.chat_id,
     message.user_id,
   ))
 
-  if chat == None or len(chat.data) == 0:
+  if chat == None:
     raise HTTPException(status_code=404, detail="Chat not found for that user.")
 
   supabase = await get_client()
-  response = await (
+
+  context = question(message.message)
+  summary = chat["old_history_summary"]
+  conversation = chat["recent_history"]
+
+  prompt = build_prompt(
+    question=message.message,
+    context=context,
+    summary=summary,
+    conversation=conversation,
+  )
+
+  response = generate(prompt)
+
+  assistant_message_date = datetime.now().isoformat()
+
+  db_response = await (
     supabase.table(MESSAGES_TABLE)
-    .insert({
-      "id": generate(),
-      "chat_id": message.chat_id,
-      "user_id": message.user_id,
-      "type": message.type,
-      "message": message.message,
-    })
+    .insert([
+      {
+        "id": nanoid(),
+        "chat_id": message.chat_id,
+        "user_id": message.user_id,
+        "type": message.type,
+        "message": message.message,
+        "created_at": user_message_date,
+      },
+      {
+        "id": nanoid(),
+        "chat_id": message.chat_id,
+        "user_id": message.user_id,
+        "type": MessageType.assistant,
+        "message": response,
+        "created_at": assistant_message_date,
+      },
+    ])
     .execute()
   )
 
-  return response
+  return dict(
+    data=response,
+  )
 
 async def list_messages(chat: Chat, page: int, page_size: int):
   range = page_to_range(page, page_size)
--- src/rag/query.py
+++ src/rag/query.py
@@ -1,6 +1,4 @@
 from src.rag.load import load_retriever
-from src.llm.llm import generate
-from src.llm.prompt import build_prompt
 
 def question(question: str, top_k: int = 4) -> str:
   retriever = load_retriever()
@@ -10,13 +8,4 @@ def question(question: str, top_k: int = 4) -> str:
 
   context = "\n\n".join([node.get_content() for node in nodes])
 
-  prompt = build_prompt(
-    question=question,
-    context=context,
-    summary=None,
-    conversation=None,
-  )
-
-  response = generate(prompt)
-
-  return str(response)
+  return context
