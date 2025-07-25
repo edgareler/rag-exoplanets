561956e89532a9621a410a34994fef4105e2bc24
Edgar Eler <eler@edgar.systems>
2025-07-25 07:16:22 -0300

    Implement basic API key middleware

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from src.chats.router import router as router_chats
 from src.messages.router import router as router_messages
 from src.llm.llm import load_model, warm_up, unload_model
 from src.rag.load import load_index, load_retriever, unload_index
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from starlette.middleware.base import BaseHTTPMiddleware
+
+load_dotenv()
+
+RAG_API_KEY = os.getenv("RAG_API_KEY")
+
+class APIKeyMiddleware(BaseHTTPMiddleware):
+  async def dispatch(self, request: Request, call_next):
+    header_api_key = request.headers.get("RAG-API-Key")
+    if header_api_key != RAG_API_KEY:
+      return JSONResponse(
+        status_code=401,
+        content={"error": "Unauthorized: Invalid API key"}
+      )
+    return await call_next(request)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -20,6 +38,8 @@ async def lifespan(app: FastAPI):
   unload_model()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(APIKeyMiddleware)
 
 @app.get("/")
 def read_root():
